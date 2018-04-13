132bbe3b31add9c65d3e58dfb230a07e27383e7b
babudzhi <babudhi@lanit.ru>
2018-04-13 17:23:40 +0300

    asdas

--- src/ru/babudzhi/HelloWorld.java
+++ src/ru/babudzhi/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, new branch");
         System.out.println("Недурно, сэр, недурно.");
+        System.out.println("1111");
     }
 }
