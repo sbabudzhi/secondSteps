2aa68c159be39b28cc8fd8f03dbf958d65b3f597
babudzhi <babudhi@lanit.ru>
2018-04-13 14:22:28 +0300

    Change for master 2

--- src/ru/babudzhi/HelloWorld.java
+++ src/ru/babudzhi/HelloWorld.java
@@ -2,7 +2,7 @@ package ru.babudzhi;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello Master");
         System.out.println("Недурно, сэр, недурно.");
     }
 }
